d8b34be8dafdda14a9d40a60133710685ef26449
Imen-Frigui <89009627+Imen-Frigui@users.noreply.github.com>
2023-02-09 16:51:55 +0100

    Add files via upload

---
+++ src/Controller/ClubController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ClubController extends AbstractController
+{
+    #[Route('/club', name: 'app_club')]
+    public function index(): Response
+    {
+        return $this->render('club/index.html.twig', [
+            'controller_name' => 'ClubController',
+        ]);
+    }
+    #[Route('/list', name: 'app_list')]
+    public function list(): Response
+    {
+        $formations = array(
+            array(
+                'ref' => 'f1',
+                'Titre' => 'Formation Symfony
+            4',
+                'Description' => 'formation pratique',
+                'nb_participants' => 19
+            ),
+            array(
+                'ref' => 'f2',
+                'Titre' => 'Formation SOA',
+                'Description' => 'formation theorique',
+                'nb_participants' => 0
+            ),
+            array(
+                'ref' => 'f3',
+                'Titre' => 'Formation Angular',
+                'Description' => 'formation theorique',
+                'nb_participants' => 12
+            )
+        );
+        return $this->render('club/list.html.twig', [
+            'f' => $formations,
+        ]);
+    }
+}
---
+++ src/Controller/TeacherController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TeacherController extends AbstractController
+{
+    #[Route('/teacher', name: 'app_teacher')]
+    public function index(): Response
+    {
+        return $this->render('teacher/index.html.twig', [
+            'controller_name' => 'TeacherController',
+        ]);
+    }
+}
